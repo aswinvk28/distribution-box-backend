113b34f82eaa29941ecd532fb9039969a93165fe
Aswin Vijayakumar <aswinkvj@gmail.com>
2021-03-08 15:16:15 +0000

    develop includes changes from release and dropped from git archive

--- actions/database.php
+++ actions/database.php
@@ -1,12 +1,35 @@
 <?php
 
-$connectionParams = array(
-    'path' => '/home/project/database/power_distribution_box.db',
-    'user' => 'admin',
-    'password' => 'admin',
-    'host' => 'localhost',
-	'driver' => 'pdo_sqlite',
-	'memory' => FALSE
-);
+namespace PowerDistribution\Access;
 
-$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+class Database 
+{
+    private static $conn;
+    public $config;
+    
+    public function __construct()
+    {
+        $connectionParams = array(
+            'path' => '/home/project/database/power_distribution_box.db',
+            'user' => 'admin',
+            'password' => 'admin',
+            'host' => 'localhost',
+            'driver' => 'pdo_sqlite',
+            'memory' => FALSE
+        );
+        
+        static::$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+
+        static::$config = parse_ini_file(dirname(__FILE__) . '../site.ini');
+    }
+
+    public function getConfig() 
+    {
+        return static::$config;
+    }
+
+    public static function getConnection()
+    {
+        return static::$conn;
+    }
+}
