a3089a58c7b41919ef2df9fd2fcad2186307bf11
Aswin Vijayakumar <aswinkvj@gmail.com>
2021-03-12 17:24:35 +0000

    build files removed

--- build_server.sh
+++ build_server.sh
@@ -30,9 +30,10 @@ if [ "$file_mode" == 'A' ] || [ "$file_mode" == 'M' ]; then
     file_name=`basename "$file_path"`
     echo "$directory_name"
 
-    if [ ! -d "${directory_name}" ]; then
-        lftp -c "open -u javawebmaster,$STAGING_FTP_PASSWORD 134.209.85.146; set ssl:verify-certificate no; mirror -R ./$directory_name /buildyourbox/$directory_name"
-    else
+    if [ -d "${directory_name}" ]; then
+        lftp -c "open -u javawebmaster,$STAGING_FTP_PASSWORD 134.209.85.146; set ssl:verify-certificate no; mkdir /buildyourbox/$directory_name"
+    fi
+    if [ -f "${file_path}" ]; then
         lftp -c "open -u javawebmaster,$STAGING_FTP_PASSWORD 134.209.85.146; set ssl:verify-certificate no; cd /buildyourbox/$directory_name; put $file_path"
     fi
 elif [ "$file_mode" == 'D' ]; then
